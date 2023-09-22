e1c5577910e12ef034cf4ae78b768705c6df1d16
Павел Баканов <bakanov2210@icloud.com>
2023-09-22 21:50:13 +0300

    added lab1

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,10 +1,23 @@
 ﻿namespace Lab1
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Введите числа через пробел:");
+            var input = Console.ReadLine();
+            var list = input?.Split(' ').Select(int.Parse);
+
+            Console.WriteLine("Введите n: ");
+            var minCnt = int.Parse(Console.ReadLine()!);
+
+            FilterList(list!, minCnt).ToList().ForEach(x => Console.Write(x + " "));        
         }
+
+        public static IEnumerable<int> FilterList(IEnumerable<int> list, int minCnt) =>
+            list
+            .Where(x => list
+            .GroupBy(x => x)
+            .ToDictionary(x => x.Key, x => x.Count())[x] > minCnt);
     }
 }
